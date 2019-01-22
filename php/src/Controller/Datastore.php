549c97afda7281c31cd73b64793703444312fe27
skxeve <skxeve@gmail.com>
2019-01-22 23:41:15 +0900

    table list.

--- php/src/Controller/Datastore.php
+++ php/src/Controller/Datastore.php
@@ -9,6 +9,53 @@ class Datastore
     {
         $dao = new Dao;
         $result = $dao->execQuery('SELECT * FROM Person');
+        echo <<<EOM
+<html>
+<head>
+<meta charset="utf-8">
+<title>List Person</title>
+<style>
+table {
+    border: solid 2px #222222;
+    border-collapse: collapse;
+}
+th, td {
+    border: solid 1px #ff2222;
+}
+</style>
+</head>
+<body>
+<table style="">
+<thead>
+<tr>
+<th>key</th>
+<th>name</th>
+<th>sex</th>
+<th>age</th>
+<th>height</th>
+<th>marriaged</th>
+<th>(option)</th>
+</tr>
+</thead>
+<tbody>
+EOM;
+        foreach ($result as $item) {
+            echo '<tr>';
+            $keyObj = $item->key();
+            $path = $keyObj->path();
+            $propertyMap = $item->get();
+            echo '<td>' . json_encode($path) . '</td>';
+            foreach (['name', 'sex', 'age', 'height', 'marriaged'] as $key) {
+                echo '<td>' . json_encode($propertyMap[$key]) . '</td>';
+            }
+            echo '<td>' . json_encode($item->meanings()) . '</td>';
+            echo '</tr>' . PHP_EOL;
+        }
+        echo <<<EOM
+</tbody>
+</table>
+<hr>
+EOM;
         $this->printDump($result, '#ccccff');
         $i = 0;
         foreach ($result as $item) {
@@ -16,6 +63,10 @@ class Datastore
             echo '<br/>' . PHP_EOL;
             $this->printDump($item, '#ccffff');
         }
+        echo <<<EOM
+</body>
+</html>
+EOM;
     }
 
     public function readAction()
